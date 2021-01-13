cead25049dcbf7651e6bf96dbbc617991e65fba6
Antonio Ailton Gonçalves <antonioailton723@gmail.com>
2021-01-13 20:19:22 +0000

    exclusão de duas listas de exercicio

--- FundamentoDeProgramacaoOrientadaAObejetos/ListaDeExercicios/Lista1/ConversaoTemperatura.java
+++
@@ -1,7 +0,0 @@
-class ConversaoTemperatura{
-	public static void main(String args[]){
-	double celsius = 32.4;
-	double  fahrenheit = (celsius*9+160)/5;
-	System.out.println("A temperatura de " +celsius+ "°c sendo a conversao em  Fahrenheit " +fahrenheit+ "°f");
-	}
-}
--- FundamentoDeProgramacaoOrientadaAObejetos/ListaDeExercicios/Lista1/Divisao.java
+++
@@ -1,8 +0,0 @@
-class Divisao{
-	public static void main(String args[]){
-		int num1 = 10;
-		int num2 = 2;
-		int divisao = num1/num2;
-		System.out.println("O resultado de " +num1+ " / " +num2+ " e igual a " +divisao);
-	}
-}
